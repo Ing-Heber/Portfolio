d7393483f42488e6e3e6113a885d7aedd149ef4f
Héber Herrera <ing.heberh@gmail.com>
2024-03-22 23:13:36 -0600

    add actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+const LOCALHOST_URL = `http://localhost:4321`,
+LIVE_URL = "https://Ing-Heber.github.io",
+SCRIPT = process.env.npm_lifecycle_script || "",
+isBuild = SCRIPT.includes('astro build');
+let BASE_URL = LOCALHOST_URL;
+
+ if (isBuild) BASE_URL = LIVE_URL;
+ 
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: BASE_URL
+  });
